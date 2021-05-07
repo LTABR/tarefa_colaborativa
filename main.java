a08dc853e3ef37fc97a6a9e81c84aa47419b1596
LTABR <83617420+LTABR@users.noreply.github.com>
2021-05-06 21:13:56 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,34 +1,26 @@
 import javax.swing.JOptionPane;
 public class main extends Calculo_Circunferencia{
-    public static void main(String[] args) { 
-        boolean aux1 = true;
-        int aux2;  
+    public static void main(String[] args) {
+        int aux;  
         do {
-            aux2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção\n1 - Retangulo\n2 - Circulo\n0 - Sair"));
-            switch (aux2) {
+            aux = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção\n1 - Retângulo\n2 - Circunferência\nOutro - Sair"));
+            switch (aux) {
                 case 1:
                     Calculo_Retangulo retangulo = new Calculo_Retangulo();
                     retangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog(null, "Entre com o valor da base")));
                     retangulo.setAltura(Double.parseDouble(JOptionPane.showInputDialog(null, "Entre com o valor da altura")));
                     retangulo.setArea(retangulo.calculaArea(retangulo.getAltura(), retangulo.getBase()));
                     retangulo.setPerimetro(retangulo.calculaPerimetro(retangulo.getAltura(), retangulo.getBase()));
-                    JOptionPane.showMessageDialog(null, "Valor da área do retangulo: " + retangulo.getArea() + "\nValor do perimetro do retangulo: " + retangulo.getPerimetro());
+                    JOptionPane.showMessageDialog(null, "Valor da área do retângulo: " + retangulo.getArea() + "\nValor do perímetro do retângulo: " + retangulo.getPerimetro());
                     break;
                 case 2:
                     Calculo_Circunferencia circulo = new Calculo_Circunferencia();
                     circulo.setRaio(Double.parseDouble(JOptionPane.showInputDialog(null, "Entre com o valor do raio")));
                     circulo.setArea(circulo.calculaArea(circulo.getRaio()));
                     circulo.setPerimetro(circulo.calculaPerimetro(circulo.getRaio()));
-                    JOptionPane.showMessageDialog(null, "Valor da área do circulo: " + circulo.getArea() + "\nValor do perimetro do circulo: " + circulo.getPerimetro());
+                    JOptionPane.showMessageDialog(null, "Valor da área da circunferência: " + circulo.getArea() + "\nValor do perímetro da circunferência: " + circulo.getPerimetro());
                     break;
-                case 0:
-                    aux1 = false;
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Valor inválido");
-                    break;
-            }
-        
-        } while(aux1 == true);
+            }        
+        } while(aux > 0 && aux <= 2);
     } 
 }
